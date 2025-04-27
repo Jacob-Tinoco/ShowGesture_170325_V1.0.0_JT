f60d9087003306b8dd3a9d1bbcb24e4beb985c6f
Jacob Tinoco <158215329+Jacob-Tinoco@users.noreply.github.com>
2025-04-26 22:03:31 -0600

    Add files via upload

---
+++ ShowGesture_170325_V1.0.0_JT.py
@@ -0,0 +1,11 @@
+"""
+Aqui busco crear un script que reconozca el gesto usando GestureRecognitionV1.2.0_JT.py
+y lo muestre en pantalla usando inmagenes 
+
+este sera el update para que sea interactivo el reconocimiento de los gestos para niños y desconocidos del temma 
+admeas es bueno pensar e un log para el registro de eventos y usarlo en una AI 
+
+
+
+
+"""
